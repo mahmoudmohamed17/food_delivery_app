0e3bda5c4451a8bfa1e97a48afdbfe27bc1154be
Mahmoud Mohamed <ma5975986@gmail.com>
2025-06-15 14:15:16 +0300

    Add SVG assets and update app_assets.dart with new image references

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,160 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesAppLogo
+  /// assets/images/app_logo.png
+  static const String imagesAppLogo = "assets/images/app_logo.png";
+
+  /// Assets for imagesArrow
+  /// assets/images/arrow.svg
+  static const String imagesArrow = "assets/images/arrow.svg";
+
+  /// Assets for imagesArrowDown
+  /// assets/images/arrow_down.svg
+  static const String imagesArrowDown = "assets/images/arrow_down.svg";
+
+  /// Assets for imagesBell
+  /// assets/images/bell.svg
+  static const String imagesBell = "assets/images/bell.svg";
+
+  /// Assets for imagesCall
+  /// assets/images/call.svg
+  static const String imagesCall = "assets/images/call.svg";
+
+  /// Assets for imagesCar
+  /// assets/images/car.svg
+  static const String imagesCar = "assets/images/car.svg";
+
+  /// Assets for imagesCard
+  /// assets/images/card.svg
+  static const String imagesCard = "assets/images/card.svg";
+
+  /// Assets for imagesCemara
+  /// assets/images/cemara.svg
+  static const String imagesCemara = "assets/images/cemara.svg";
+
+  /// Assets for imagesCheck
+  /// assets/images/check.svg
+  static const String imagesCheck = "assets/images/check.svg";
+
+  /// Assets for imagesCreditCard
+  /// assets/images/credit_card.svg
+  static const String imagesCreditCard = "assets/images/credit_card.svg";
+
+  /// Assets for imagesCross
+  /// assets/images/cross.svg
+  static const String imagesCross = "assets/images/cross.svg";
+
+  /// Assets for imagesDonate
+  /// assets/images/donate.svg
+  static const String imagesDonate = "assets/images/donate.svg";
+
+  /// Assets for imagesEllipseDown
+  /// assets/images/ellipse_down.svg
+  static const String imagesEllipseDown = "assets/images/ellipse_down.svg";
+
+  /// Assets for imagesEllipseUp
+  /// assets/images/ellipse_up.svg
+  static const String imagesEllipseUp = "assets/images/ellipse_up.svg";
+
+  /// Assets for imagesFilter
+  /// assets/images/filter.svg
+  static const String imagesFilter = "assets/images/filter.svg";
+
+  /// Assets for imagesGrids
+  /// assets/images/grids.svg
+  static const String imagesGrids = "assets/images/grids.svg";
+
+  /// Assets for imagesHeart
+  /// assets/images/heart.svg
+  static const String imagesHeart = "assets/images/heart.svg";
+
+  /// Assets for imagesHome
+  /// assets/images/home.svg
+  static const String imagesHome = "assets/images/home.svg";
+
+  /// Assets for imagesInfo
+  /// assets/images/info.svg
+  static const String imagesInfo = "assets/images/info.svg";
+
+  /// Assets for imagesLogo
+  /// assets/images/logo.svg
+  static const String imagesLogo = "assets/images/logo.svg";
+
+  /// Assets for imagesLogout
+  /// assets/images/logout.svg
+  static const String imagesLogout = "assets/images/logout.svg";
+
+  /// Assets for imagesMastercard
+  /// assets/images/mastercard.svg
+  static const String imagesMastercard = "assets/images/mastercard.svg";
+
+  /// Assets for imagesMenu
+  /// assets/images/menu.svg
+  static const String imagesMenu = "assets/images/menu.svg";
+
+  /// Assets for imagesMessage
+  /// assets/images/message.svg
+  static const String imagesMessage = "assets/images/message.svg";
+
+  /// Assets for imagesMicOff
+  /// assets/images/mic_off.svg
+  static const String imagesMicOff = "assets/images/mic_off.svg";
+
+  /// Assets for imagesMinus
+  /// assets/images/minus.svg
+  static const String imagesMinus = "assets/images/minus.svg";
+
+  /// Assets for imagesMore
+  /// assets/images/more.svg
+  static const String imagesMore = "assets/images/more.svg";
+
+  /// Assets for imagesPaypal
+  /// assets/images/paypal.svg
+  static const String imagesPaypal = "assets/images/paypal.svg";
+
+  /// Assets for imagesPlus
+  /// assets/images/plus.svg
+  static const String imagesPlus = "assets/images/plus.svg";
+
+  /// Assets for imagesQuestion
+  /// assets/images/question.svg
+  static const String imagesQuestion = "assets/images/question.svg";
+
+  /// Assets for imagesSearch
+  /// assets/images/search.svg
+  static const String imagesSearch = "assets/images/search.svg";
+
+  /// Assets for imagesSend
+  /// assets/images/send.svg
+  static const String imagesSend = "assets/images/send.svg";
+
+  /// Assets for imagesShare
+  /// assets/images/share.svg
+  static const String imagesShare = "assets/images/share.svg";
+
+  /// Assets for imagesSmile
+  /// assets/images/smile.svg
+  static const String imagesSmile = "assets/images/smile.svg";
+
+  /// Assets for imagesTag
+  /// assets/images/tag.svg
+  static const String imagesTag = "assets/images/tag.svg";
+
+  /// Assets for imagesUser
+  /// assets/images/user.svg
+  static const String imagesUser = "assets/images/user.svg";
+
+  /// Assets for imagesVector
+  /// assets/images/vector.svg
+  static const String imagesVector = "assets/images/vector.svg";
+
+  /// Assets for imagesVisa
+  /// assets/images/visa.svg
+  static const String imagesVisa = "assets/images/visa.svg";
+
+  /// Assets for imagesWatch
+  /// assets/images/watch.svg
+  static const String imagesWatch = "assets/images/watch.svg";
+}
