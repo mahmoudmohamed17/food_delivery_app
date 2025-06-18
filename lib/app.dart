5d31d302a026dc3228827de0e81b367dc274f057
Mahmoud Mohamed <ma5975986@gmail.com>
2025-06-18 13:25:22 +0300

    Implement routing with GoRouter and restructure main app entry point

---
+++ lib/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery_app/core/routing/app_routing.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      routerConfig: router,
+    );
+  }
+}
